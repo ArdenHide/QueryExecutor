81ce15f01bc7303df5acb4c6ae9129ee4f255c1d
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-04-21 21:19:57 +0300

    refactor (#19)

--- QueryExecutorTests/QueryExecutor.cs
+++ QueryExecutorTests/QueryExecutor.cs
@@ -2,9 +2,8 @@
 
 public class QueryExecutor : Executor
 {
-    public QueryExecutor(string cmdText) : base(cmdText) { }
+    public QueryExecutor(string cmdText) : base(cmdText, "Server=localhost;Database=mydatabase;User Id=myusername;Password=mypassword;") { }
 
     public override bool EnableAWSXRay { get; set; } = false;
-
-    public override string GetConnectionString() => "Server=localhost;Database=mydatabase;User Id=myusername;Password=mypassword;";
+    public string ConnectionString { get; set; } = "Server=localhost;Database=mydatabase;User Id=myusername;Password=mypassword;";
 }
