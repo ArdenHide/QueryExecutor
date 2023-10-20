a2d9e5521b9011e12685390d14d03c75443d8a9d
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-10-20 13:21:52 +0300

    Create model of output data (#35)
    * - add model
    - refactor code
    - update tests
    
    * refactor ResponseBuilder

---
+++ src/QueryExecutor/Utils/ResponseBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using QueryExecutor.Models;
+
+namespace QueryExecutor.Utils;
+
+public static class ResponseBuilder
+{
+    private static readonly JObject EmptyResponseError = new JObject
+    {
+        { "error", "Received empty string from DB." }
+    };
+
+    private static readonly JObject InvalidJsonError = new JObject
+    {
+        { "error", "Received data from DB cannot convert to JSON." }
+    };
+
+    public static Response BuildResponse(string jsonResponse)
+    {
+        if (string.IsNullOrWhiteSpace(jsonResponse))
+        {
+            return new Response(Status.Error, EmptyResponseError);
+        }
+
+        try
+        {
+            var response = JToken.Parse(jsonResponse);
+            return new Response(Status.Success, response);
+        }
+        catch (JsonException)
+        {
+            return new Response(Status.Error, InvalidJsonError);
+        }
+    }
+}
