5c978a66cf99ac819ecb699c44b80e2a372ad171
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-20 14:23:29 +0300

    fix tests

---
+++ tests/QueryExecutor.Tests/Mock/MockData.cs
@@ -0,0 +1,7 @@
+﻿namespace QueryExecutor.Tests.Mock;
+
+internal static class MockData
+{
+    internal const string ConnectionString = "Server=localhost;";
+    internal const string CmdText = "SELECT * FROM TableName FOR JSON AUTO";
+}
